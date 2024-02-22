92eaa4627d6dda2c19bbc4b8ef2d812e2b7cbfbb
JSiuda1 <jakkub68@gmail.com>
2024-02-22 21:39:27 +0100

    Initial project structure

---
+++ components/hello_world/hello_world.h
@@ -0,0 +1,3 @@
+// Copyright 2024 PWrInSpace, Jakub Siuda
+
+void hello_world(void);
---
+++ main/main.c
@@ -0,0 +1,21 @@
+// Copyright 2024 PWrInSpace, Kuba
+#include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "hello_world.h"
+
+
+void app_main(void) {
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
+
+    uint32_t gpio_level = 1;
+    while(1) {
+        hello_world();
+
+        gpio_set_level(GPIO_NUM_2, gpio_level);
+        gpio_level = !gpio_level;
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
