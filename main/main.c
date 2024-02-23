ca00a147941f22a8a58b50de68f42205d0164ffb
JSiuda1 <jakkub68@gmail.com>
2024-02-23 23:10:31 +0100

    Add new setting to clang, add test build and clang to actions

--- main/main.c
+++ main/main.c
@@ -1,16 +1,16 @@
 // Copyright 2024 PWrInSpace, Kuba
 #include <stdint.h>
+
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
 #include "hello_world.h"
-
 
 void app_main(void) {
     gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
 
     uint32_t gpio_level = 1;
-    while(1) {
+    while (1) {
         hello_world();
 
         gpio_set_level(GPIO_NUM_2, gpio_level);
