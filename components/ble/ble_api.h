f17d981e9fa949c514d0db4d4abf264b5b4e4782
kgliwinski <259282@student.pwr.edu.pl>
2024-02-25 22:34:59 +0100

    Successfully ran tests

---
+++ components/ble/ble_api.h
@@ -0,0 +1,96 @@
+// Copyright 2023 PWr in Space, Krzysztof Gliwiński
+#pragma once
+
+#include <stdint.h>
+
+#include "ble_gap_conf.h"
+#include "ble_gatt_conf.h"
+#include "esp_bt.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_main.h"
+#include "nvs_flash.h"
+#include "sdkconfig.h"
+
+/*!
+ * \file ble_api.h
+ * \brief BLE API - contains all BLE functions and structures
+ * ass well as attributes of GATT - unified in one structure
+ */
+
+/* LSB
+ <-------------------------------------------------------------------------------->
+ MSB */
+// first uuid, 16bit, [12],[13] is the value
+// second uuid, 32bit, [12], [13], [14], [15] is the value
+#define BLE_UUID_CONFIG_DEFAULT()                                           \
+  {                                                                         \
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, \
+        0xEE, 0x00, 0x00, 0x00, 0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00,   \
+        0x80, 0x00, 0x10, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00,               \
+  }
+
+// slave connection min interval, Time = min_interval * 1.25 msec
+// slave connection max interval, Time = max_interval * 1.25 msec
+// TEST_MANUFACTURER_DATA_LEN,
+// &test_manufacturer[0],
+#define BLE_ADV_DATA_CONFIG_DEFAULT()                                          \
+  {                                                                            \
+    .set_scan_rsp = false, .include_name = true, .include_txpower = false,     \
+    .min_interval = 0x0006, .max_interval = 0x0010, .appearance = 0x00,        \
+    .manufacturer_len = 0, .p_manufacturer_data = NULL, .service_data_len = 0, \
+    .p_service_data = NULL, .service_uuid_len = sizeof(adv_service_uuid128),   \
+    .p_service_uuid = adv_service_uuid128,                                     \
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),      \
+  }
+
+// TEST_MANUFACTURER_DATA_LEN,
+// &test_manufacturer[0],
+#define BLE_SCAN_RSP_DATA_CONFIG_DEFAULT()                                  \
+  {                                                                         \
+    .set_scan_rsp = true, .include_name = true, .include_txpower = true,    \
+    .appearance = 0x00, .manufacturer_len = 0, .p_manufacturer_data = NULL, \
+    .service_data_len = 0, .p_service_data = NULL,                          \
+    .service_uuid_len = sizeof(adv_service_uuid128),                        \
+    .p_service_uuid = adv_service_uuid128,                                  \
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),   \
+  }
+
+#define BLE_ADV_PARAMS_CONFIG_DEFAULT()                                 \
+  {                                                                     \
+    .adv_int_min = 0x20, .adv_int_max = 0x40, .adv_type = ADV_TYPE_IND, \
+    .own_addr_type = BLE_ADDR_TYPE_PUBLIC, .channel_map = ADV_CHNL_ALL, \
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,             \
+  }
+
+/*!
+ * \brief BLE status enum
+ */
+typedef enum { BLE_OK = 0, BLE_HARDWARE_INIT_ERR, BLE_ERR } ble_err_t;
+
+/*!
+ * \brief Main BLE configuration struct.
+ * Contains gatts and gap configuration
+ */
+typedef struct {
+  ble_gatts_t *gatt_config;
+  ble_gap_t *gap_config;
+  esp_bt_controller_config_t bt_cfg;
+} ble_config_t;
+
+/*!
+ * \brief Initiates all necessary esp hardware related to bluetooth
+ */
+ble_err_t ble_esp_hardware_init(ble_config_t *ble);
+
+/*!
+ * \brief Initiates the whole bluetooth stack, as well as gap ble_gatts_t and
+ * ble_gap_t configuration
+ */
+ble_err_t ble_init(ble_config_t *ble);
+
+/*!
+ * \brief Converts ble_err_t to string
+ * \param[in] err - error to convert
+ * \return string representation of error
+ */
+const char *ble_err_to_string(ble_err_t err);
---
+++ components/ble/ble_gatt_conf.h
@@ -0,0 +1,84 @@
+// Copyright 2023 PWr in Space, Krzysztof Gliwiński
+#pragma once
+
+#include <stdint.h>
+
+#include "esp_err.h"
+#include "esp_gatt_common_api.h"
+#include "esp_gatt_defs.h"
+#include "esp_gatts_api.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+
+/*!
+ * \file ble_gatt_conf.h
+ * \brief BLE GATT configuration structure
+ */
+
+/*!
+ * \brief Maximum number of GATT profiles
+ */
+#define GATTS_PROFILES_NUM_MAX 16
+
+/*!
+ * \brief Maximum number of GATT attributes in db
+ * \note Limited for ESP-IDF for ESP32 to 16, for ESP32-S3 is 256 (due to DRAM limitations)
+ */
+#if CONFIG_IDF_TARGET_ESP32
+#define GATTS_ATTRIBUTES_DB_MAX_NUM 16
+#elif CONFIG_IDF_TARGET_ESP32S3
+#define GATTS_ATTRIBUTES_DB_MAX_NUM 256
+#else  // default value
+#define GATTS_ATTRIBUTES_DB_MAX_NUM 16
+#endif
+
+/*!
+ * \brief Handle GATT events
+ * \param[in] event GATT event
+ * \param[in] param GATT event parameters
+ */
+typedef void (*ble_gatts_event_handler)(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if,
+                                        esp_ble_gatts_cb_param_t *param);
+
+/*!
+ * \brief BLE GATT configuration structure
+ */
+typedef struct {
+    //   esp_gatts_attr_db_t *gatt_db;
+    esp_gatts_cb_t gatts_cb;
+    uint16_t gatts_if;
+    uint16_t conn_id;
+    uint16_t service_handle;
+    esp_gatt_srvc_id_t service_id;
+    uint16_t char_handle;
+    esp_bt_uuid_t char_uuid;
+    esp_gatt_perm_t perm;
+    esp_gatt_char_prop_t property;
+    uint16_t descr_handle;
+    esp_bt_uuid_t descr_uuid;
+    esp_gatts_attr_db_t gatt_db[GATTS_ATTRIBUTES_DB_MAX_NUM];
+} ble_gatts_profile_t;
+
+/*!
+ * \brief Main gatts configuration structure
+ * \param[in] profiles_num Number of profiles
+ * \param[in] profiles Array of profiles
+ * \param[in] event_handler_cb Event handler callback
+ */
+typedef struct {
+    uint16_t profiles_num;
+    ble_gatts_event_handler event_handler_cb;
+    ble_gatts_profile_t profiles[GATTS_PROFILES_NUM_MAX];
+} ble_gatts_t;
+
+/*!
+ * \brief Initialize the BLE GATT module
+ * \param[in] gatts_conf GATT configuration structure
+ */
+esp_err_t ble_gatt_init(ble_gatts_t *gatts_conf);
+
+/*!
+ * \brief Register the BLE GATT module
+ * \param[in] gatts_conf GATT configuration structure
+ */
+esp_err_t ble_gatt_register_event(ble_gatts_t *gatts_conf);
